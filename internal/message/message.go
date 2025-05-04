0f31fd6e3d81b867281ee076a167775ebaac0b30
berlins07 <suberlin.sinaga@bababos.com>
2025-05-04 22:57:18 +0700

    Simplify the message handler by directly split the function to handle data from indodax and tokocrypto

--- internal/message/message.go
+++ internal/message/message.go
@@ -13,180 +13,107 @@ import (
 type RawMessage struct {
 	serverName string
 	serverUrl  string
-	data       []byte
+	data       map[string]any
 	coins      []config.Asset
 	receivedAt time.Time
 }
 
-type ProcessedMessage struct {
-	ServerName  string
-	ServerUrl   string
-	Data        any
-	CoinName    string
-	ChannelName string
-	ReceivedAt  time.Time
-}
-
-type DataProcessor interface {
-	StoreData() error
-}
-
 func CreateMessage(serverName, serverURL string, coin []config.Asset, data []byte) (*RawMessage, error) {
-	if data == nil {
-		return nil, fmt.Errorf("no data found")
+	var jsonMsg map[string]any
+	if err := json.Unmarshal(data, &jsonMsg); err != nil {
+		log.Printf("[message.go] Error parsing message: %v", err)
+		return nil, err
 	}
+
 	return &RawMessage{
 		serverName: serverName,
 		serverUrl:  serverURL,
-		data:       data,
+		data:       jsonMsg,
 		coins:      coin,
 		receivedAt: time.Now(),
 	}, nil
 }
 
-// the message must be handled in the raw level
-func (raw *RawMessage) HandleMessage(ctx context.Context) error {
-
-	var jsonMsg map[string]any
-	if err := json.Unmarshal(raw.data, &jsonMsg); err != nil {
-		log.Printf("[message.go] Error parsing message: %v", err)
-		return err
+func (raw *RawMessage) HandleIndodaxMessage(ctx context.Context) error {
+	result, ok := raw.data["result"].(map[string]any)
+	if !ok {
+		return fmt.Errorf("invalid message format, expecting has result key")
 	}
-	// log.Printf("[message.go] the message is: %v", jsonMsg)
-
-	// var channelName string = ""
+	channel, ok := result["channel"].(string)
+	if !ok {
+		return fmt.Errorf("invalid message format, result has no channel key")
+	}
 	var coinName string = ""
-	// var data_ any
-	if raw.serverName == "indodax" {
-		result, ok := jsonMsg["result"].(map[string]any)
-		if !ok {
-			return fmt.Errorf("invalid message format, expecting has result key")
-		}
-		channel, ok := result["channel"].(string)
-		if !ok {
-			return fmt.Errorf("invalid message format, result has no channel key")
-		}
-
-		if strings.HasPrefix(channel, "market:order-book-") {
-			// channelName = "order_book"
-			var lastPart string = ""
-			parts := strings.Split(channel, "-")
-			if len(parts) > 0 {
-				lastPart = parts[len(parts)-1]
+	parts := strings.Split(channel, "-")
+	var lastPart string = ""
+	var volumeName string = ""
+	if len(parts) > 0 {
+		lastPart = parts[len(parts)-1]
+	}
+	for _, coin := range raw.coins {
+		if strings.ReplaceAll(coin.Name, "_", "") == lastPart {
+			coinName = coin.Name
+			coinParts := strings.Split(coinName, "_")
+			if len(coinParts) > 0 {
+				volumeName = coinParts[0] + "_volume"
 			}
-			for _, asset := range raw.coins {
-				if strings.ReplaceAll(asset.Name, "_", "") == lastPart {
-					coinName = asset.Name
-				}
-			}
-			var data_ *OrderBookIndodax
-			if err := json.Unmarshal(raw.data, &data_); err != nil {
-				return fmt.Errorf("error unmarshalling order book message for %s from %s order book channel: %v", coinName, raw.serverName, err)
-			}
-			// dt = &ProcessedMessage{
-			// 	ServerName:  raw.serverName,
-			// 	ServerUrl:   raw.serverUrl,
-			// 	Data:        data_,
-			// 	ChannelName: channelName,
-			// 	CoinName:    coinName,
-			// }s
-			return nil
-			// log.Printf("Finish printing out the orderbook data: %s", data)
-		} else if strings.HasPrefix(channel, "market:trade-activity-") {
-			// channelName = "trade"
-			parts := strings.Split(channel, "-")
-			var lastPart string = ""
-			if len(parts) > 0 {
-				lastPart = parts[len(parts)-1]
-			}
-			for _, coin := range raw.coins {
-				if strings.ReplaceAll(coin.Name, "_", "") == lastPart {
-					coinName = coin.Name
-				}
-			}
-			var data_ *TradeActivityIndodax
-			if err := json.Unmarshal(raw.data, &data_); err != nil {
-				return fmt.Errorf("error unmarshalling trade activity message for %s from %s order book channel: %v", coinName, raw.serverName, err)
-			}
-			// return &ProcessedMessage{
-			// 	ServerName:  raw.serverName,
-			// 	ServerUrl:   raw.serverUrl,
-			// 	Data:        data_,
-			// 	ChannelName: channelName,
-			// 	CoinName:    coinName,
-			// },
-			return nil
-		}
-	} else if raw.serverName == "tokoCrypto" {
-		ch, ok := jsonMsg["e"].(string)
-		if !ok {
-			return fmt.Errorf("invalid message, it has no key 'e'")
-		}
-		symbol, _ := jsonMsg["s"].(string)
-		// eventTime, _ := jsonMsg["E"].(int64)
-		// bid, _ := jsonMsg["b"].([]any)
-		// ask, _ := jsonMsg["a"].([]any)
-
-		if ch == "depthUpdate" {
-			// channelName = "order_book"
-			for _, coin := range raw.coins {
-				if strings.ReplaceAll(coin.Name, "_", "") == strings.ToLower(symbol) {
-					coinName = coin.Name
-				}
-			}
-			// var data_ *OrderBookTokoCrypto
-			// if err := json.Unmarshal(raw.data, &data_); err != nil {
-			// 	return nil, fmt.Errorf("error unmarshalling order book message for %s from %s order book channel: %v", coinName, raw.serverName, err)
-			// }
-
-			// data_ := &OrderBookTokoCrypto{
-			// 	Pair:      coinName,
-			// 	EventTime: eventTime,
-			// 	Bid:       bid,
-			// 	Ask:       ask,
-			// }
-
-			// return &ProcessedMessage{
-			// 	ServerName:  raw.serverName,
-			// 	ServerUrl:   raw.serverUrl,
-			// 	Data:        data_,
-			// 	ChannelName: channelName,
-			// 	CoinName:    coinName,
-			// },
-
-			return nil
-		} else if ch == "trade" {
-			// channelName = "trade"
-			for _, coin := range raw.coins {
-				if strings.ReplaceAll(coin.Name, "_", "") == strings.ToLower(symbol) {
-					coinName = coin.Name
-				}
-			}
-			// var data_ *TradeActivityTokoCrypto
-			// if err := json.Unmarshal(raw.data, &data_); err != nil {
-			// 	return nil, fmt.Errorf("error unmarshalling trade activity message for %s from %s trade channel: %v", coinName, raw.serverName, err)
-			// }
-			// data_ := &TradeActivityTokoCrypto{
-			// 	Pair: coinName,
-			// 	// Price: ,
-			// }
-			// return &ProcessedMessage{
-			// 	ServerName:  raw.serverName,
-			// 	ServerUrl:   raw.serverUrl,
-			// 	Data:        data_,
-			// 	ChannelName: channelName,
-			// 	CoinName:    coinName,
-			// },
-			return nil
 		}
 	}
+	if strings.HasPrefix(channel, "market:order-book-") {
+		data_ := &OrderBookIndodax{
+			ExchangeName:    raw.serverName,
+			PairSymbol:      coinName,
+			Data:            raw.data,
+			CreatedDatetime: raw.receivedAt,
+		}
+		data_.FinalizeData(volumeName)
 
-	// return &ProcessedMessage{
-	// 	ServerName:  raw.serverName,
-	// 	ServerUrl:   raw.serverUrl,
-	// 	Data:        data_,
-	// 	ChannelName: channelName,
-	// 	CoinName:    coinName,
-	// },
+		return nil
+
+	} else if strings.HasPrefix(channel, "market:trade-activity-") {
+
+		data_ := &TradeActivityIndodax{
+			ExchangeName: raw.serverName,
+			PairSymbol:   coinName,
+			Data:         raw.data,
+		}
+		data_.FinalizeData()
+		return nil
+	}
+	return nil
+}
+
+func (raw *RawMessage) HandleTokoCryptoMessage(ctx context.Context) error {
+	ch, ok := raw.data["e"].(string)
+	if !ok {
+		return fmt.Errorf("invalid message, it has no key 'e'")
+	}
+	symbol, _ := raw.data["s"].(string)
+	var coinName string = ""
+	for _, coin := range raw.coins {
+		if strings.ReplaceAll(coin.Name, "_", "") == strings.ToLower(symbol) {
+			coinName = coin.Name
+		}
+	}
+	// eventTime, _ := raw.data["E"].(int64)
+	// bid, _ := raw.data["b"].([]any)
+	// ask, _ := raw.data["a"].([]any)
+
+	if ch == "depthUpdate" {
+		data_ := &OrderBookTokoCrypto{
+			ExchangeName: raw.serverName,
+			PairSymbol:   coinName,
+			Data:         raw.data,
+		}
+		data_.FinalizeData()
+	} else if ch == "trade" {
+		data_ := &TradeActivityTokoCrypto{
+			ExchangeName: raw.serverName,
+			PairSymbol:   coinName,
+			Data:         raw.data,
+		}
+		data_.FinalizeData()
+	}
+
 	return nil
 }
